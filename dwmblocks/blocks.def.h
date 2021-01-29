3edc260365afa9d81439fee5242a35a096ca802a
oicleevan <oicleevan@protonmail.com>
2021-01-29 10:14:43 -0500

    update dwmblocks config

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
 	{" | ", 	" uname -r",		360,			0},
 	{"",		"/home/eo/.local/bin/pacupdate", 360,		0},
 	{"", 	"free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
-	{"", 	"date '+%a %b %d (%l:%M%p )'",	5, 0},
+	{"", 	"date '+%a %b %d (%l:%M%p)'",	5, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
